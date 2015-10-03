b39feedfe1e123610d50d9f1aec2d2e8bb9e1f2e
zibber <coodd11009@gmail.com>
2015-10-03 03:52:39 +0000

    20151002

---
+++ 7th_week/jquery_exercise/js/script.js
@@ -0,0 +1,119 @@
+/*
+
+Instructions:
+
+---- PART 1 ----
+Start by removing that pirate flag (#pirate).
+
+Next, add a gradient to the whole page by applying the "gradient" class to the body.
+
+---- PART 2 ----
+Now, to make things a little more complicated, let's activate the "Start" button:
+
+When the user clicks the "Start" button, change the text of "status" to read "GO". 
+Let's also change the background color of "status" to green.
+Change the text of the "Start" button to read "Stop".
+
+When the user clicks the button again, revert all of the above. 
+So "status" should read "STOP", it's background color should be red, and the button itself should read "Start" again.
+
+Hint: you will want to store the current state of the button in a variable.
+
+---- PART 3 ----
+Now that we have stored the status of our button, let's show the user an image if the mouse over our green "GO" box.
+
+If the user has activated the button, toggle the visibility of id "cat" to reveal our animated leopard.
+
+*/
+
+// part 1
+$("#pirate").hide();
+
+$("body").addClass("gradient")
+
+// part 2
+
+$("#toggle").click(function() {
+    if ($("#toggle").html() == "Start") {
+   $("#status").html("GO");
+   $("#status").css("background-color","green") ;
+   $("#toggle").html("Stop");
+   $("#cat").show();
+   $("#status").mouseenter(function() {
+       $("#pirate").show();
+     })
+     $("#status").mouseleave(function() {
+       $("#pirate").hide();
+     })
+    } else {
+   $("#status").html("STOP");
+   $("#status").css("background-color","red");
+   $("#toggle").html("Start");
+   $("#cat").hide();
+   $("#status").mouseenter(function() {
+       $("#pirate").hide();
+     })
+     $("#status").mouseleave(function() {
+       $("#pirate").hide();
+     })
+    }
+    
+});
+
+
+/* var img = new Image();
+var div = document.getElementById('foo');
+
+img.onload = function() {
+  div.appendChild(img);
+}; */
+
+var img = 'img/pirate.gif';
+
+
+
+
+// part 3
+
+/* $("#status").mouseenter(function() {
+    if ($("#toggle").html() == "Start") {
+   $("#cat").show();
+
+   
+});
+
+
+// $("body").html("")
+
+
+
+
+
+  /* confirm("YOU MAKE IT!");
+   prompt("Please grade here") */
+
+
+
+
+
+
+/* $("#toggle").click(function() {
+    if ($("#toggle").html() == "Start") {
+   $("#status").html("GO");
+   $("#status").css("background-color","green") ;
+   $("#toggle").html("Stop");
+ }
+});
+
+
+/* $("#status").mouseover(function() {
+  if ($("#status").css() == "background-color","green") {
+   $("#status").addClass("#pirate")
+  }
+}
+
+<img onmouseover="bigImg(this)" src="cat.gif" alt="Smiley">
+
+$("body").addClass("#cat")
+
+*/
---
+++ scripts.js
@@ -0,0 +1,39 @@
+var counter = 0;
+
+
+
+$("h1").click(function() {
+   console.log("hellow world"); 
+    if (counter < 10) {
+        
+    }
+   //mousedown
+   //mouseup
+   //mouseenter
+   //mouseleave
+   counter++;
+   console.log(counter)
+
+});
+
+
+
+
+var person = {
+    name: "Junhao",
+    age: 24,
+    gender: "male",
+    location: "Broklyn, NY" 
+    // this is a note... each line have to start with -->//
+}
+// Download the compressed, production jQuery 1.11.3
+// http://jquery.com/download/
+
+console.log("test")
+
+function addNumbers(num1, num2) {
+    var sum = num1 + num2;
+    return sum;
+}
+
+// toggleClass()
